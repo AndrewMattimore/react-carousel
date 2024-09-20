2e60b6ce332e1911c16b5f052f8d0c1d77c502c5
AndrewMattimore <a.mattimore@erasolutions.us>
2024-09-20 17:54:14 -0500

    test

--- carousel/src/App.js
+++ carousel/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        right account?
+       another try
         </p>
         <a
           className="App-link"
