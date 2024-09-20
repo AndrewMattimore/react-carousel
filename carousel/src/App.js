c6eefbf0bdbb9a1ecd94790fc46f8d2e2f44b24b
AndrewMattimore <sgtmanhood@gmail.com>
2024-09-20 17:52:26 -0500

    right account?

--- carousel/src/App.js
+++ carousel/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         completely new test
+        right account?
         </p>
         <a
           className="App-link"
