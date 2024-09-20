9cbea3b715c4d63062fa2f7dc072aa2b895d1435
AndrewMattimore <sgtmanhood@gmail.com>
2024-09-20 17:37:06 -0500

    merge into main branch

--- carousel/src/App.js
+++ carousel/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing from new branch
+         merging to main branch
         </p>
         <a
           className="App-link"
